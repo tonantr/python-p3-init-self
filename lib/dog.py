c467a491efd98c5b6f2a5fb51653c32282d42f03
tonan <tonantruong@hotmail.com>
2023-07-24 09:21:31 -0400

    object initialization

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed='Mutt'):
+        self.name = name
+        self.breed = breed
+
+    # def bark(self):
+    #     print('Woof!')
+    
+    # def showing_self(self):
+    #     return self
+    
+
+    
